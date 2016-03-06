a3b3b283b00b07afd0f67567613d900496ca2b45
Vincent Wong <vincentw@live.ca>
2016-03-06 02:21:17 -0800

    Hangman now requires internet as it calls an external API to fetch a word

--- HangmanGame.py
+++ HangmanGame.py
@@ -1,19 +1,32 @@
 # HANGMAN game
-# An offline Hangman game against the computer!
+# An offline Hangman game!
 
-# Note: this uses an offline version of Webster's Dictionary
-# Check out Wordnik API for an online version of a dictionary
+import random
+import json
+# Wordnik API
+from wordnik import *
+apiUrl = 'http://api.wordnik.com/v4'
+apiKey = '3c50e7b5e1e604f09956ea7cca5010850216618e67e05bf76'
+client = swagger.ApiClient(apiKey, apiUrl)
+wordsApi = WordsApi.WordsApi(client)
 
-
-#import webster dictionary module/lib
+def getRandomWord():
+    return wordsApi.getRandomWord(includePartOfSpeech='noun',
+                                 excludePartOfSpeech='conjunction',
+                                 hasDictionaryDef='true',
+                                 minCorpusCount=55000,
+                                 maxCorpusCount=-1,
+                                 minDictionaryCount=1,
+                                 maxDictionaryCount=-1,
+                                 minLength=7,
+                                 maxLength=-1).word
 
 def new_game():
     global answer, answerWithSpace, word, guesses, misses, notGuessed
     guesses = ""
     misses = ""
     word = ""
-    #get_word_from_lib store in answer
-    answer = notGuessed =  "hangman".upper() #just for testing
+    answer = notGuessed = getRandomWord().upper()
     answerWithSpace = answer.replace(""," ")[1:-1]
     for ch in range (0, len(answer)):
         word += "_ "
